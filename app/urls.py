4a0516fef55eea59a463d25edd5ce9e4ea6e242b
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-06-18 08:28:56 +0300

    fix: fixing url navbar

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index,name='index'),
-    path('post',views.post,name='post'),
-    path('profile',views.profile,name='profile'),
+    path('post/',views.post,name='post'),
+    path('login/',views.loginpage,name='login'),
+    path('register/',views.register,name='register'),
+    path('profile/',views.profile,name='profile'),
 ]
